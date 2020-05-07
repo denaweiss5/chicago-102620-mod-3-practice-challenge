dd1c4ff5de558006d574078c9e198069479a2cb4
Rupa <rupa211984@gmail.com>
2020-05-07 16:11:26 -0700

    Add a test

--- test/core_test.js
+++ test/core_test.js
@@ -1,10 +1,39 @@
-var chai = require('chai');
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+const path = require('path');
+const htmlFilePath = `file:${path.join(__dirname, '/../index.html')}`;
+const firstDancer = {
+  id: 1,
+  name: "Carlton",
+  image: "https://i.imgur.com/iM8ybeC.gif",
+  description: "20 years of experience in all forms of dance. Known for shiny outfits.",
+  likes: 26,
+  feedback: [
+    "Nice moves!",
+    "Never stop never stopping"
+  ]
+};
 
-// var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function() {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });
+let browser;
+let page;
+
+before(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+});
+
+describe('View first dancer', () => {
+  it('has the name Carlton in an h2', async () => {
+    await page.goto(htmlFilePath);
+    const h2 = await page.evaluate(() => document.querySelector('#dancer-name').textContent.trim());
+
+    expect(h2).to.equal(firstDancer.name);
+  }).timeout(10000);
+
+  it('shows the number of likes', async () => {
+    await page.goto(htmlFilePath);
+    const likes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
+
+    expect(likes).to.equal(firstDancer.likes);
+  }).timeout(10000);
+});
