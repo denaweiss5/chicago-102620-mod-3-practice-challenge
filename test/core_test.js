fbb9b40c070a9295a5b675ceeee9cf08fcb8d090
Rupa <rupa211984@gmail.com>
2020-05-07 17:14:04 -0700

    Add core tests

--- test/core_test.js
+++ test/core_test.js
@@ -1,10 +1,65 @@
-var chai = require('chai');
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+const path = require('path');
+const htmlFilePath = `file:${path.join(__dirname, '/../index.html')}`;
+const json = require('../db.json');
+const firstDancer = json.dancers[0];
 
-// var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function() {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });
+let browser;
+let page;
+
+before(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto(htmlFilePath);
+});
+
+describe('View first dancer', () => {
+  it('has the name Carlton in an h2', async () => {
+    const h2 = await page.evaluate(() => document.querySelector('#dancer-name').textContent.trim());
+
+    expect(h2).to.equal(firstDancer.name);
+  }).timeout(10000);
+
+  it('shows the number of likes', async () => {
+    const likes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
+
+    expect(parseInt(likes, 10)).to.equal(firstDancer.likes);
+  }).timeout(10000);
+
+  it('shows the description', async () => {
+    const desc = await page.evaluate(() => document.querySelector('#dancer-description').textContent.trim());
+
+    expect(desc).to.equal(firstDancer.description);
+  }).timeout(10000);
+
+  it('shows the feedback in LIs', async () => {
+    const feedback = await page.evaluate(() => document.querySelectorAll('.feedback ul li'));
+    const array = Array.from(feedback).map(el => el.textContent);
+
+    array.forEach((fb, idx) => {
+      expect(fb).to.equal(firstDancer.feedback[idx]);
+    });
+  }).timeout(10000);
+
+  it('increases the likes (persistently) when clicking a button', async () => {
+    const oldLikes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
+
+    await page.click('#like');
+    await page.goto(htmlFilePath, { waitUntil: 'networkidle2' });
+
+    const likes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
+
+    expect(parseInt(likes, 10)).to.equal(parseInt(oldLikes, 10) + 1);
+  }).timeout(10000);
+
+  it('adds feedback via a form', async () => {
+    const feedback = 'testing test test';
+    await page.type('#new-feedback', feedback);
+    await page.click('input[type="submit"]');
+
+    const lastFeedback = await page.evaluate(() => document.querySelector('.feedback ul li:last-of-type').textContent.trim());
+
+    expect(lastFeedback).to.equal(feedback);
+  }).timeout(10000);
+});
