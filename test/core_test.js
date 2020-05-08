0382174cb85a0391ce4824b37e12b2e67738e52e
Rupa <rupa211984@gmail.com>
2020-05-07 17:12:15 -0700

    Test core deliverables

--- test/core_test.js
+++ test/core_test.js
@@ -2,17 +2,8 @@ const puppeteer = require('puppeteer');
 const { expect } = require('chai');
 const path = require('path');
 const htmlFilePath = `file:${path.join(__dirname, '/../index.html')}`;
-const firstDancer = {
-  id: 1,
-  name: "Carlton",
-  image: "https://i.imgur.com/iM8ybeC.gif",
-  description: "20 years of experience in all forms of dance. Known for shiny outfits.",
-  likes: 26,
-  feedback: [
-    "Nice moves!",
-    "Never stop never stopping"
-  ]
-};
+const json = require('../db.json');
+const firstDancer = json.dancers[0];
 
 let browser;
 let page;
@@ -20,20 +11,55 @@ let page;
 before(async () => {
   browser = await puppeteer.launch();
   page = await browser.newPage();
+  await page.goto(htmlFilePath);
 });
 
 describe('View first dancer', () => {
   it('has the name Carlton in an h2', async () => {
-    await page.goto(htmlFilePath);
     const h2 = await page.evaluate(() => document.querySelector('#dancer-name').textContent.trim());
 
     expect(h2).to.equal(firstDancer.name);
   }).timeout(10000);
 
   it('shows the number of likes', async () => {
-    await page.goto(htmlFilePath);
     const likes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
 
-    expect(likes).to.equal(firstDancer.likes);
+    expect(parseInt(likes, 10)).to.equal(firstDancer.likes);
+  }).timeout(10000);
+
+  it('shows the description', async () => {
+    const desc = await page.evaluate(() => document.querySelector('#dancer-description').textContent.trim());
+
+    expect(desc).to.equal(firstDancer.description);
+  }).timeout(10000);
+
+  it('shows the feedback in LIs', async () => {
+    const feedback = await page.evaluate(() => document.querySelectorAll('.feedback ul li'));
+    const array = Array.from(feedback).map(el => el.textContent);
+
+    array.forEach((fb, idx) => {
+      expect(fb).to.equal(firstDancer.feedback[idx]);
+    });
+  }).timeout(10000);
+
+  it('increases the likes (persistently) when clicking a button', async () => {
+    const oldLikes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
+
+    await page.click('#like');
+    await page.goto(htmlFilePath, { waitUntil: 'networkidle2' });
+
+    const likes = await page.evaluate(() => document.querySelector('#like-count').textContent.trim());
+
+    expect(parseInt(likes, 10)).to.equal(parseInt(oldLikes, 10) + 1);
+  }).timeout(10000);
+
+  it('adds feedback via a form', async () => {
+    const feedback = 'testing test test';
+    await page.type('#new-feedback', feedback);
+    await page.click('input[type="submit"]');
+
+    const lastFeedback = await page.evaluate(() => document.querySelector('.feedback ul li:last-of-type').textContent.trim());
+
+    expect(lastFeedback).to.equal(feedback);
   }).timeout(10000);
 });
